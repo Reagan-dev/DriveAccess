37732d97accd20e9807d217dcb323bc17e80d02a
Reagan-dev <reagansimiyu38@gmail.com>
2025-08-26 20:38:26 +0300

    did some changes in user model

--- motorvehicle_leasing/driveaccess_backend/accounts/models.py
+++ motorvehicle_leasing/driveaccess_backend/accounts/models.py
@@ -44,9 +44,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    @property
-    def id(self):
-        return self.user_id
+   
 
     def save(self, *args, **kwargs):
         # Ensure password is hashed before saving
